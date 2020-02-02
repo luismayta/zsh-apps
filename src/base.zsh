57435e5e61100ba3a993cbb15ebf13b265d8ff5e
@slovacus <slovacus@gmail.com>
2020-02-02 13:48:21 -0500

    chore: implement install rsync (#3)

--- src/base.zsh
+++ src/base.zsh
@@ -1,6 +1,15 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+function rsync::install {
+    if ! type -p brew > /dev/null; then
+        message_error "it's neccesary brew, add: luismayta/zsh-brew"
+    fi
+    message_info "Installing rsync for ${apps_package_name}"
+    brew install rsync
+    message_success "Installed rsync ${apps_package_name}"
+}
+
 function apps::node::install {
     if ! type nvm > /dev/null; then
         message_warning "is Neccesary install nvm, please install antibody bundle luismayta/zsh-nvm branch:develop"
@@ -56,6 +65,7 @@ function apps::has_dependences {
     return
 }
 
+if ! type -p rsync > /dev/null; then rync::install; fi
 if [ "$(apps::has_node)" -eq 0 ]; then apps::node::install; fi
 if [ "$(apps::has_yarn)" -eq 0 ]; then apps::yarn::install; fi
 if [ "$(apps::has_nativefier)" -eq 0 ]; then apps::nativefier::install; fi
