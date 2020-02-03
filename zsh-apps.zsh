9acd813477e7e29465a52a10da2488da8b722221
@slovacus <slovacus@gmail.com>
2020-02-03 04:59:05 -0500

    chore: load apps.zsh for install apps(#3)

--- zsh-apps.zsh
+++ zsh-apps.zsh
@@ -7,10 +7,12 @@
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
 #
-apps_package_name="apps"
-
-APPS_PLUGIN_DIR="$(dirname "${0}":A)"
-APPS_SOURCE_PATH="${APPS_SOURCE_PATH}"/src
+export apps_package_name="apps"
+APPS_PLUGIN_DIR="$(dirname "${0}")"
+export APPS_SOURCE_PATH="${APPS_PLUGIN_DIR}"/src
 
 # shellcheck source=/dev/null
 source "${APPS_SOURCE_PATH}"/base.zsh
+
+# shellcheck source=/dev/null
+source "${APPS_SOURCE_PATH}"/apps.zsh
