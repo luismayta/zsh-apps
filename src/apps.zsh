c6d2d1dd8ce0ffaf14ab6b415a38b9b73b50c5cc
@slovacus <slovacus@gmail.com>
2020-05-06 16:43:11 -0500

    chore: implement app calendar (#12)

--- src/apps.zsh
+++ src/apps.zsh
@@ -10,3 +10,15 @@ function apps::jira::install {
                        "https://endustria.atlassian.net/secure/RapidBoard.jspa" && \
             rsync -a ./Jira-darwin-x64/Jira.app/ /Applications/Jira.app/ && cd - || exit
 }
+
+function apps::calendar::install {
+    local name name_path
+    name="GoogleCalendar"
+    name_path="/Applications/${name}.app"
+    mkdir -p "${name_path}"
+    cd /var/tmp/ || cd - && \
+            nativefier --name "${name}" \
+                       --single-instance \
+                       "https://calendar.google.com/calendar/r" && \
+            rsync -a ./"${name}"-darwin-x64/"${name}".app/ "${name_path}/" && cd - || exit
+}
