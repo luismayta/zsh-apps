6a41c6257ba23c20c3f5b8a540ef4f7a886e2424
@slovacus <slovacus@gmail.com>
2020-05-07 06:00:36 -0500

    improvement: message for brew and nvm (#14)

--- zsh-apps.zsh
+++ zsh-apps.zsh
@@ -11,6 +11,10 @@ export APPS_PACKAGE_NAME="apps"
 APPS_PLUGIN_DIR="$(dirname "${0}")"
 export APPS_SOURCE_PATH="${APPS_PLUGIN_DIR}"/src
 
+export APPS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export APPS_MESSAGE_NVM="Please install nvm or use antibody bundle luismayta/zsh-nvm branch:develop"
+export APPS_MESSAGE_NOT_FOUND="this not found installed"
+
 # shellcheck source=/dev/null
 source "${APPS_SOURCE_PATH}"/base.zsh
 
