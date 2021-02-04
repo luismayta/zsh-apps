6c6cdb90c0c9eb3081dc1e6b6b1cabdc676147f1
@slovacus <slovacus@gmail.com>
2021-02-03 22:48:16 -0500

    refactor: structure code for code of zsh (#22)

---
+++ pkg/base.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function apps::upgrade {
+    message_warning "method not implement"
+}
+
+function apps::install {
+    message_warning "method not implement"
+}
+
+function apps::post_install {
+    message_warning "method not implement"
+}
--- zsh-apps.zsh
+++ zsh-apps.zsh
@@ -7,37 +7,15 @@
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
 #
-export APPS_PACKAGE_NAME="apps"
-APPS_PLUGIN_DIR="$(dirname "${0}")"
-export APPS_SOURCE_PATH="${APPS_PLUGIN_DIR}"/src
 
-export APPS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
-export APPS_MESSAGE_NVM="Please install nvm or use antibody bundle luismayta/zsh-nvm branch:develop"
-export APPS_MESSAGE_NOT_FOUND="this not found installed"
+# shellcheck disable=SC2034  # Unused variables left for readability
+ZSH_APPS_PATH=$(dirname "${0}")
 
 # shellcheck source=/dev/null
-source "${APPS_SOURCE_PATH}"/base.zsh
-
-# cross::os functions for osx and linux
-function cross::os {
-
-    case "${OSTYPE}" in
-        darwin*)
-            # shellcheck source=/dev/null
-            source "${APPS_SOURCE_PATH}"/osx.zsh
-            ;;
-        linux*)
-            # shellcheck source=/dev/null
-            source "${APPS_SOURCE_PATH}"/linux.zsh
-            ;;
-    esac
-
-}
-
-cross::os
+source "${ZSH_APPS_PATH}"/config/main.zsh
 
 # shellcheck source=/dev/null
-source "${APPS_SOURCE_PATH}"/utils.zsh
+source "${ZSH_APPS_PATH}"/internal/main.zsh
 
 # shellcheck source=/dev/null
-source "${APPS_SOURCE_PATH}"/apps.zsh
+source "${ZSH_APPS_PATH}"/pkg/main.zsh
