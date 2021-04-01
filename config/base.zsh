bcc9b41e1352bfa97dd6b3d86ae5b65c6d5e569f
@slovacus <slovacus@gmail.com>
2021-03-31 19:24:02 -0500

    refactor: install and validation apps (#26)

--- config/base.zsh
+++ config/base.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export APPS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export APPS_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/brew branch:develop"
 export APPS_PACKAGE_NAME=apps
