41205d190a16fa10b7c21f47c06117d4f7a5857f
Luis Mayta <luis@hadenlabs.com>
2023-02-27 15:06:44 -0500

    chore: implement sync up standard code (#49)
    * refactor: upgrade task actions and configuration (#49)
    
    * feat: implement version project (#49)
    
    * style: format files using prettier (#49)
    
    * build: generate files (#49)

--- config/base.zsh
+++ config/base.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export APPS_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/brew branch:develop"
 export APPS_PACKAGE_NAME=apps
 export APPS_PATH_TMP=/var/tmp
 export APPS_USER_AGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"
