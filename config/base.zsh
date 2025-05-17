4d6ae160892ab0a332ac23ec19a8faba3d6520ea
Luis Mayta <luis@hadenlabs.com>
2025-05-17 05:09:51 -0300

    feat ✨ (apps): implement tools for developer (#183)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- config/base.zsh
+++ config/base.zsh
@@ -2,13 +2,50 @@
 # -*- coding: utf-8 -*-
 
 export APPS_PACKAGE_NAME=apps
+
+export APPS_IDE_TOOLS=(raycast tig meld)
+
+export APPS_JETBRAINS=(
+    jetbrains-toolbox
+    intellij-idea-ce
+)
+export APPS_COMMUNICATION=(discord)
+export APPS_KNOWLEDGE=(obsidian drawio)
+export APPS_DEVOPS=(
+  ngrok
+  unite
+  orbstack
+  multipass
+)
+
+export APPS_BROWSER=(
+  arc
+  brave
+)
+
+export APPS_ANDROID=(
+  android-studio
+  android-platform-tools
+  genymotion
+)
+
+export APPS_MOBILE=(
+  watchman
+  fastlane
+)
+
+export APPS_PROJECT_MANAGEMENT=(
+  jira-cli
+)
+
 export APPS_PACKAGES=(
-    raycast
-    tig
-    discord
-    meld
-    obsidian
-    unite
-    ngrok
-    drawio
+    "${APPS_IDE_TOOLS[@]}"
+    "${APPS_JETBRAINS[@]}"
+    "${APPS_COMMUNICATION[@]}"
+    "${APPS_KNOWLEDGE[@]}"
+    "${APPS_DEVOPS[@]}"
+    "${APPS_BROWSER[@]}"
+    "${APPS_ANDROID[@]}"
+    "${APPS_MOBILE[@]}"
+    "${APPS_PROJECT_MANAGEMENT[@]}"
 )
